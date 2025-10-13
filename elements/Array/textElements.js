f4eb353702acaa524d57dd28b5c9c0678eea10c9
Lindarleon <159889211+Lindarleon@users.noreply.github.com>
2025-10-14 01:39:19 +0200

    Create textElements.js

---
+++ elements/Array/textElements.js
@@ -0,0 +1,32 @@
+// elements/textElements.js
+export const textElements = [
+  {
+    name: 'Paragraph',
+    tag: 'p',
+    defaultContent: 'This is a paragraph.',
+    styles: {
+      color: '#000000',
+      fontSize: '16px',
+    },
+  },
+  {
+    name: 'Heading 1',
+    tag: 'h1',
+    defaultContent: 'Heading 1',
+    styles: {
+      color: '#000000',
+      fontSize: '32px',
+      fontWeight: 'bold',
+    },
+  },
+  {
+    name: 'Heading 2',
+    tag: 'h2',
+    defaultContent: 'Heading 2',
+    styles: {
+      color: '#000000',
+      fontSize: '24px',
+      fontWeight: '600',
+    },
+  },
+];
