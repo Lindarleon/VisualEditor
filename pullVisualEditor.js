069ed07a66eb6d94ed90d1473d21feb11aa1c9ac
Lindarleon <159889211+Lindarleon@users.noreply.github.com>
2025-10-14 02:00:09 +0200

    Create pullVisualEditor.js

---
+++ pullVisualEditor.js
@@ -0,0 +1,7 @@
+import { sidebarRender } from './sidebar/elements.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sidebar = document.getElementById('sidebar');
+  const sidebarContent = sidebarRender();
+  sidebar.appendChild(sidebarContent);
+});
