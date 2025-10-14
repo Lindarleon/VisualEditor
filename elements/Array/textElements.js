4f4d19aeb264ba876c0f0682f144da507ee7e8b1
Lindarleon <159889211+Lindarleon@users.noreply.github.com>
2025-10-14 02:30:22 +0200

    Update textElements.js

--- elements/Array/textElements.js
+++ elements/Array/textElements.js
@@ -1,5 +1,4 @@
 export const textElements = [
-  { id: 'text1', name: 'Paragraph', tag: 'p', defaultContent: 'This is a paragraph.' },
-  { id: 'text2', name: 'Heading 1', tag: 'h1', defaultContent: 'Heading 1' },
-  { id: 'text3', name: 'Heading 2', tag: 'h2', defaultContent: 'Heading 2' },
+  { id: 'text1', label: 'Text Paragraph' },
+  { id: 'text2', label: 'Heading' }
 ];
