f3f68a12033e8ae214f5e0314170580a7d8e6ba9
Lindarleon <159889211+Lindarleon@users.noreply.github.com>
2025-10-14 02:31:05 +0200

    Update imageElement.js

--- elements/Array/imageElement.js
+++ elements/Array/imageElement.js
@@ -1,4 +1,4 @@
 export const imageElements = [
-  { id: 'image1', name: 'Placeholder Image', tag: 'img', attributes: { src: 'https://via.placeholder.com/150', alt: 'Placeholder Image' } },
-  { id: 'image2', name: 'Rounded Image', tag: 'img', attributes: { src: 'https://via.placeholder.com/100', alt: 'Rounded Image' } },
+  { id: 'image1', label: 'Image Placeholder' },
+  { id: 'image2', label: 'Logo' }
 ];
